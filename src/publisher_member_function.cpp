3310d724bb00248aa7388f2c628564d6449cbbe5
Sanchit kedia <sanchit@terpmail.umd.edu>
2022-11-03 00:06:47 -0400

    Modifying talker and listener programs

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -36,7 +36,7 @@ class MinimalPublisher : public rclcpp::Node {
  private:
   void timer_callback() {
     auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
+    message.data = "Hey, Terrapins! Count with Me " + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
