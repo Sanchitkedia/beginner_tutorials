f1a8a6f18c5d310dfe79c6fbb7efc5e7f5ab78fa
Sanchit kedia <sanchit@terpmail.umd.edu>
2022-11-05 12:23:05 -0400

    Changing publisher file to create server and change talker output on service call

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -1,51 +1,49 @@
-// Copyright 2016 Open Source Robotics Foundation, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "beginner_tutorials/srv/string_change.hpp"  
 
 #include <chrono>
 #include <functional>
 #include <memory>
 #include <string>
 
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-
 using namespace std::chrono_literals;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
- * member function as a callback from the timer. */
-
-class MinimalPublisher : public rclcpp::Node {
- public:
-  MinimalPublisher() : Node("minimal_publisher"), count_(0) {
+class MinimalPublisher : public rclcpp::Node
+{
+public:
+  MinimalPublisher()
+  : Node("minimal_publisher"), count_(0)
+  {
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-    timer_ = this->create_wall_timer(
-        500ms, std::bind(&MinimalPublisher::timer_callback, this));
+    timer_ = this->create_wall_timer(500ms, std::bind(&MinimalPublisher::timer_callback, this));
+    server_ = this->create_service<beginner_tutorials::srv::StringChange>("string_change", std::bind(&MinimalPublisher::string_change_callback, this, std::placeholders::_1, std::placeholders::_2));
   }
 
- private:
+private:
   void timer_callback() {
     auto message = std_msgs::msg::String();
-    message.data = "Hey, Terrapins! Count with Me " + std::to_string(count_++);
+    message.data = msg + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::Service<beginner_tutorials::srv::StringChange>::SharedPtr server_;
   size_t count_;
+  std::string msg = "Hey, Terrapins! Count with Me ";
+
+  bool string_change_callback(const beginner_tutorials::srv::StringChange::Request::SharedPtr request, const beginner_tutorials::srv::StringChange::Response::SharedPtr response) {
+    msg = request->input;
+    response->output = msg;
+    RCLCPP_INFO(this->get_logger(), "Incoming request\nOutput String is: %s", msg.c_str());
+    RCLCPP_INFO(this->get_logger(), "Sending response to talker: %s", msg.c_str());
+    return true;
+  }
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char * argv[])
+{
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalPublisher>());
   rclcpp::shutdown();
